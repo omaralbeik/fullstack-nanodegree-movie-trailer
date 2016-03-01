161e54a2c366c0c72be76ea67142fd9c1f9802a7
Omar Albeik <omaralbeik@gmail.com>
2016-03-01 19:42:08 +0200

    refactor: code refactored after 1st review

--- media.py
+++ media.py
@@ -4,7 +4,8 @@ import webbrowser
 class Media():
     """Media is the parent class for Movie and TVShow classes, it contains all generic information for both"""
 
-    def __init__(self, title, story_line, poster_image_url, trailer_youtube_url, year):
+    def __init__(self, title, story_line, poster_image_url,
+                 trailer_youtube_url, year):
         self.title = title
         self.story_line = story_line
         self.poster_image_url = poster_image_url
@@ -15,7 +16,8 @@ class Media():
 class Movie(Media):
     """Movie class is a child class of Media, specifically for holding movies information"""
 
-    def __init__(self, title, story_line, poster_image_url, trailer_youtube_url, year, duration):
+    def __init__(self, title, story_line, poster_image_url,
+                 trailer_youtube_url, year, duration):
         Media.__init__(self, title, story_line,
                        poster_image_url, trailer_youtube_url, year)
         self.duration = duration
@@ -24,7 +26,8 @@ class Movie(Media):
 class TVShow(Media):
     """TVShow class is a child class of Media, specifically for holding tv shows information"""
 
-    def __init__(self, title, story_line, poster_image_url, trailer_youtube_url, year, seasons_count):
+    def __init__(self, title, story_line, poster_image_url,
+                 trailer_youtube_url, year, seasons_count):
         Media.__init__(self, title, story_line,
                        poster_image_url, trailer_youtube_url, year)
         self.seasons_count = seasons_count
